fd9517ad49eb7e8780f016054ca42dfaa5155200
anabsena <anabsenaf@gmail.com>
2026-05-01 22:03:35 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,17 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://www.projetarmais.com.br',
+      'https://projetarmais.com.br',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  app.setGlobalPrefix('api');
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
